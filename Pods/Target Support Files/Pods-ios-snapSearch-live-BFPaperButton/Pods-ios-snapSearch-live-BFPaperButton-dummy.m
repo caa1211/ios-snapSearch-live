012a4e1ca900ba9a52cc42dfe7b66d89642bc9e6
Carter Chang <carter@yahoo-inc.com>
2015-07-18 03:04:50 +0800

    implement ui by design

---
+++ Pods/Target Support Files/Pods-ios-snapSearch-live-BFPaperButton/Pods-ios-snapSearch-live-BFPaperButton-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ios_snapSearch_live_BFPaperButton : NSObject
+@end
+@implementation PodsDummy_Pods_ios_snapSearch_live_BFPaperButton
+@end
