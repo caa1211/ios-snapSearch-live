bfc6bfe52701ec811389e481be64b90b79cc2de1
Carter Chang <carter@yahoo-inc.com>
2015-07-21 15:36:54 +0800

    modify style

--- ios-snapSearch-live/CVViewController.mm
+++ ios-snapSearch-live/CVViewController.mm
@@ -138,17 +138,18 @@ typedef enum EFFECT_MODE : NSInteger {
     self.videoCamera.defaultAVCaptureDevicePosition = AVCaptureDevicePositionBack;
     self.videoCamera.defaultAVCaptureVideoOrientation = AVCaptureVideoOrientationPortrait;
     self.videoCamera.defaultAVCaptureSessionPreset = AVCaptureSessionPresetHigh;
-    self.videoCamera.defaultFPS = 25;
+    self.videoCamera.defaultFPS = 20;
     self.videoCamera.grayscaleMode = NO;
     self.videoCamera.delegate = self;
     
     self.cameraViewMask.dynamic = YES;
     self.cameraViewMask.blurRadius = 20;
-    self.cameraViewMask.updateInterval = 30/60;
-    self.cameraViewMask.layer.shadowColor = [[UIColor blackColor] CGColor];
-    self.cameraViewMask.layer.shadowOffset = CGSizeMake(0.0f, 0.0f);
+    self.cameraViewMask.updateInterval = 40/60;
+    self.cameraViewMask.layer.shadowColor = [[UIColor colorWithWhite:0.480 alpha:1.000] CGColor];
+    self.cameraViewMask.layer.shadowOffset = CGSizeMake(0.0f, -1.0f);
     self.cameraViewMask.layer.shadowRadius = 2.0f;
-    self.cameraViewMask.layer.shadowOpacity = 1.0f;
+    self.cameraViewMask.layer.shadowOpacity = 0.6;
+    self.cameraImageView.tintColor = [UIColor whiteColor];
     self.cameraViewMask.underlyingView = self.cameraImageView;
     
     NSArray * devices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
